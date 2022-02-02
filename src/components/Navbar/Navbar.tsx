90a468e8531e36345fb18d6cadaaae40bed9a104
Lityuk <lityuk@gmail.com>
2022-02-02 17:06:30 +0300

    devid by components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from "./Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <nav className={s.nav}>
+      <ul>
+        <li className={s.item}>Profile</li>
+        <li className={s.item}>Messages</li>
+        <li className={s.item}>News</li>
+        <li className={s.item}>Music</li>
+        <li className={s.item}>Settings</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
